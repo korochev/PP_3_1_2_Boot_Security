1fd8e3d190b3dab4091a07070e6fbfbf502a7fd9
korochev <myver33@gmail.com>
2022-07-12 12:13:56 +0300

    intellij_idea//2022.1//07.07

--- src/main/java/ru/kata/spring/boot_security/demo/models/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/User.java
@@ -1,5 +1,7 @@
 package ru.kata.spring.boot_security.demo.models;
 
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -29,7 +31,8 @@ public class User implements UserDetails {
 
     private String email;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.LAZY)
+    @Fetch(FetchMode.JOIN)
     @JoinTable(name="users_roles", joinColumns = @JoinColumn(name="role_id"), inverseJoinColumns = @JoinColumn(name =
             "user_id"))
     private Set<Role> roles;
